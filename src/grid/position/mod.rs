e2b2baec9300e730bb3f98a6cf42e5dccea01401
joe goodman <jgood1133@gmail.com>
2023-05-10 17:15:53 -0400

    made the enum and all that

--- src/grid/position/mod.rs
+++ src/grid/position/mod.rs
@@ -1,6 +1,36 @@
+// where should the grid/text/thing be displayed?
+// this enum is meant to be generic
+// that is, it should work for x and y values
+// the goal:
+// if a y value is centered, the text or grid or whatever will just be centered
+// if a x value is centered, the text or grid or whatever will just be centered
+//
+// this enum and all of its functions assume that, when drawing something,
+// the (x, y) pair provided represents the top left corner of the thing being drawn
+// (similar to the draw_rect function and unlike the draw_text function)
+#[derive(Clone, Copy)]
 pub enum Position {
-    Start,
-    End,
-    Center,
-    Pixels(f32)
+    Start, // left or top
+    End, // right or bottom
+    Center, // middle (either way)
+    Pixels(f32) // + means right or down and - means left or up
 }
+
+impl Position {
+    // TODO is there a non ass way to make this type aware that it is either x or y?
+    pub fn as_pixels(&self, width_or_height_of_thing: f32, width_or_height_of_screen: f32) -> f32 {
+        match self {
+            Position::Start => 0.0,
+            Position::End => width_or_height_of_screen - width_or_height_of_thing,
+            Position::Center => (width_or_height_of_screen - width_or_height_of_thing)/2.0,
+            Position::Pixels(offset) => *offset,
+        }
+    }
+}
+
+impl From<f32> for Position {
+    fn from(value: f32) -> Self {
+        Position::Pixels(value)
+    }
+}
+
