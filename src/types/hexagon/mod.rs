9ae07c9849087456dd44cc9f1a36c80fd1eac5f9
azazeldev <68147363+azazelcodes@users.noreply.github.com>
2025-02-16 09:58:25 +0100

    Hexagon Extravaganza
    we got universal functions!!
    now with rings, cube coords, finding your neighbours and moving around diagonally!
    thanks to
    https://www.redblobgames.com/grids/hexagons/

--- src/types/hexagon/mod.rs
+++ src/types/hexagon/mod.rs
@@ -33,9 +33,9 @@ pub fn draw_cell(
 
     // staggerinh
     let (x_adjust, y_adjust) = if pointy {
-        (if row % 2 == 0 { 0.5 * cell_width } else { 0.0 }, 0.0)
+        (if row % 2 == 0 { 0.5 * cell_width } else { 0.0 }, 0.0) // ODD-R
     } else {
-        (0.0, if col % 2 == 0 { 0.5 * cell_height } else { 0.0 })
+        (0.0, if col % 2 == 0 { 0.5 * cell_height } else { 0.0 }) // ODD-Q
     };    
 
     let (x_pos, y_pos) = (
@@ -81,5 +81,106 @@ pub fn draw_cell(
         let centered_y = y_pos - (cell_height - text_dim.height) / 2.0;
 
         draw_text(text, centered_x, centered_y, cell_height as f32 * 0.5, WHITE);
+    }
+}
+
+/*
+UNIVERSAL FUNCTIONS!! YIPPIE!
+
+(i32, i32, i32) == cube coordinates
+(i32, i32) == "normal" offset coordinates as used in draw_cell
+*/
+#[allow(dead_code)]
+pub mod unifunc {
+    pub fn to_cube(pointy: bool, row: i32, col: i32) -> (i32, i32, i32) {
+        let (q, r) = if pointy {
+            (col - (row + 1) / 2, row)
+        } else {
+            (col, row - (col + 1) / 2)
+        };
+        let s = -q-r;
+
+        (q, r, s)
+    }
+
+    pub fn from_cube(pointy: bool, q: i32, r: i32) -> (i32, i32) {
+        if pointy {
+            (q + (r - (r + 1)) / 2, r)
+        } else {
+            (q, r + (q - (q + 1)) / 2)
+        }
+    }
+
+    pub fn cube_dir(direction: usize) -> (i32, i32, i32) {
+        let vectors: [(i32, i32, i32); 7] = [
+            (0, 0, 0),                              // -> vectors[0] = center
+            (1, 0, -1), (1, -1, 0), (0, -1, 1),
+            (-1, 0, 1), (-1, 1, 0), (0, 1, -1),
+        ];    
+        /* 1 - 6   ^
+
+        1   2
+        /. .\ 
+        6 . * . 3
+        \. ./ 
+        5   4
+        
+        */
+
+        vectors[direction]
+    }
+
+    pub fn cube_add((cq, cr, cs): (i32, i32, i32), (vq, vr, vs): (i32, i32, i32)) -> (i32, i32, i32) {
+        (cq + vq, cr + vr, cs + vs)
+    }
+
+    pub fn cube_sub((aq, ar, ass): (i32, i32, i32), (bq, br, bs): (i32, i32, i32)) -> (i32, i32, i32) {
+        (aq + bq, ar + br, ass + bs) // ass =/= as
+    }
+
+    pub fn cube_mult((q, r, s): (i32, i32, i32), factor: i32) -> (i32, i32, i32) {
+        (q * factor, r * factor, s * factor)
+    }
+
+    pub fn cube_dist(a: (i32, i32, i32), b: (i32, i32, i32)) -> i32 {
+        let vector = cube_sub(a, b);
+        (vector.0.abs() + vector.1.abs() + vector.2.abs()) / 2
+    }
+
+    pub fn cube_neig(cube: (i32, i32, i32), direction: usize) -> (i32, i32, i32) {
+        cube_add(cube, cube_dir(direction))
+    }
+
+    pub fn cube_diag(cube: (i32, i32, i32), direction: usize) -> (i32, i32, i32) {
+        let vectors: [(i32, i32, i32); 7] = [
+            (0, 0, 0),                              // -> vectors[0] = center
+            (1, -2, 1), (2, -1, -1), (1, 1, -2),
+            (-1, 2, -1), (-2, 1, 1), (-1, -1, 2)
+        ];
+        /* 1 - 6   ^
+
+            1
+        6 /. .\ 2
+        . * .
+        5 \. ./ 3
+            4
+        
+        */    
+        return cube_add(cube, vectors[direction])
+    }
+
+    pub fn cube_ring(center: (i32, i32, i32), radius: i32) -> Vec<(i32, i32, i32)> { // NOT RADIUS 0!!
+        let mut results: Vec<(i32, i32, i32)> = Vec::new();
+        
+        let mut hex = cube_add(center, cube_mult(cube_dir(5), radius));
+
+        for i in 1..7 { // not 0..6 because 0 would be centers
+            for _ in 0..radius {
+                results.push(hex);
+                hex = cube_neig(hex, i);
+            }
+        }    
+
+        results
     }
 }
